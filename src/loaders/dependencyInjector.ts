ebb23ef6fb3dcdde5e0246c9a867088da2203a69
Sachin Sharma <sachin2252sharma@gmail.com>
2023-01-30 15:49:49 +0530

    fix

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,6 +1,6 @@
 import { Container } from 'typedi';
-import formData from 'form-data';
-import Mailgun from 'mailgun.js';
+// import formData from 'form-data';
+// import Mailgun from 'mailgun.js';
 import LoggerInstance from './logger';
 import agendaFactory from './agenda';
 import config from '../config';
@@ -12,12 +12,12 @@ export default ({ mongoConnection, models }: { mongoConnection; models: { name: 
     });
 
     const agendaInstance = agendaFactory({ mongoConnection });
-    const mgInstance = new Mailgun(formData);
+    // const mgInstance = new Mailgun(new formData());
 
 
     Container.set('agendaInstance', agendaInstance);
     Container.set('logger', LoggerInstance);
-    Container.set('emailClient', mgInstance.client({ key: config.emails.apiKey, username: config.emails.apiUsername }));
+    // Container.set('emailClient', mgInstance.client({ key: config.emails.apiKey, username: config.emails.apiUsername }));
     Container.set('emailDomain', config.emails.domain);
 
     LoggerInstance.info('✌️ Agenda injected into container');
